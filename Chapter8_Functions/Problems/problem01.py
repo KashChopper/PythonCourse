4f80574c11644538e81f15b8f58be304d2142f52
Aasif Bashir <syeed.ether@gmail.com>
2026-05-05 12:39:24 +0530

    Refactor factorial function to use iterative method, add sum calculation functions, and implement star pattern printing

---
+++ Chapter8_Functions/Problems/problem01.py
@@ -0,0 +1,33 @@
+# 4. Write a recursive function to calculate the sum of first n natural numbers.
+
+def sum_n(n):
+    if n <= 0:
+        return 0
+    else:
+        sum = n + sum_n(n-1)
+    return sum
+num = int(input("Enter the number: "))
+print(sum_n(num))
+
+# Using loop
+sum = 0
+for i in range(1,num+1):
+    sum = sum + i
+    
+print(sum)
+
+
+# Using function iterative method
+
+def sum(n):
+    if n < 1:
+        return 0
+    else:
+        sum = 0
+        for i in range(1, n+1):
+            sum = sum + i
+        return sum 
+print(sum(num))
+    
+
+
