62dca661761aa0440e9affd0cb1d9fda6e70415f
Aasif2709 <aasif@connectingpointstech.com>
2026-05-07 20:24:54 +0530

    Add various Python programs demonstrating lists, tuples, dictionaries, sets, conditional statements, loops, and functions
    - Implemented programs for storing fruits and marks in lists.
    - Created examples to demonstrate tuple immutability and counting elements in tuples.
    - Developed dictionary programs for Urdu to English translations and unique number detection.
    - Added conditional statements for checking eligibility and spam detection.
    - Included loop examples for multiplication tables, prime number checks, and patterns.
    - Implemented functions for Fibonacci series, factorial calculations, and sum of natural numbers.
    - Added a program to calculate the sum of digits of a number.

--- logicalPrograms/sumOfDigits.py
+++ logicalPrograms/sumOfDigits.py
@@ -1,2 +1,12 @@
-number = int(input("Enter the number: "))
 
+num = int(input("Enter the number: "))
+
+sum = 0
+
+while num > 0:
+    digit = num % 10
+    sum = sum + digit
+    num = num // 10
+
+
+print(sum)
