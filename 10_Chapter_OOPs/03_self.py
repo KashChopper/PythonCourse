d61e0b4c633107af1c69b5f9de88fef1a5a53072
Aasif Bashir <syeed.ether@gmail.com>
2026-05-09 21:17:43 +0530

    Refactor comments in Bike class for clarity and consistency

--- 10_Chapter_OOPs/03_self.py
+++ 10_Chapter_OOPs/03_self.py
@@ -1,24 +1,26 @@
 class Bike:
-    name = "Duke"
+    name = "Duke"           # class attribute
     color = "yellow"
     cc = 250
 
     def getInfo(self): # we always need to give the self as when we call it will have default one parameter
         print(f"The bike is {self.name} and the engine is {self.cc}cc.")
 
-    def greet(rr):
+    def greet(rr):   # we can give any name to the parameter but by convention we use self
         print("Assalamualikum")
 
-    @staticmethod  # f we don't need argument in function
+    @staticmethod  # we can use static method when we don't want to use self parameter and we want to call the method without creating an object
     def ask():
         print("How are you")
 
-Bike1 = Bike()
+Bike1 = Bike()          # creating an object of the class Bike and storing it in the variable Bike1
 
-Bike1.getInfo()
+Bike1.getInfo()         # calling the method getInfo using the object Bike1 and it will automatically pass the self parameter as Bike1 to the method getInfo
 # Same call can be given as 
 print("Second call")
-Bike.getInfo(Bike1)
+Bike.getInfo(Bike1)     # we can also call the method getInfo using the class name and passing the object as an argument to the method getInfo and it will work fine as we are passing the object as an argument to the method getInfo and it will automatically pass the self parameter as Bike1 to the method getInfo
 
-Bike1.greet()
+Bike1.greet()       # calling the method greet using the object Bike1 and it will automatically pass the self parameter as Bike1 to the method greet
 Bike1.ask()
+
+Bike.ask()         # calling the static method ask using the class name and it will work fine as we don't need to pass any argument to the static method ask as it doesn't have any parameter and it will automatically call the static method ask without any error
