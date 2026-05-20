d85fd9c48ed5f445df928662a90f9dc5a109844b
Aasif2709 <aasif@connectingpointstech.com>
2026-05-20 15:55:19 +0530

    Add examples for decorators and asynchronous functions

---
+++ 11_Chapter_Advanced_Python/19_Decorators.py
@@ -0,0 +1,26 @@
+"""
+A decorator is a function that takes another function as an argument and return a new function that modifies the behavior of the original function. The new function is often referred to as "decorate" function
+
+"""
+
+def greet(fn):
+    def modifiedFn():
+        print("Good morning")
+        fn()
+        print("Thanks for using this function")
+    return modifiedFn
+ 
+# def hello():
+#     print("hello world")
+
+# greet(hello)()  #This works as decorator 
+
+# Perfect way to use decorator
+
+@greet
+def hello():
+    print("Hello world")
+    
+# Now we need to call only hello function the decorator greet will that greet function as argument internally in will works as greet(hello)()
+
+hello()
