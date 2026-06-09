93945851e0dbfa9e5d50184888a59ce44d2191d8
Aasif2709 <aasif@connectingpointstech.com>
2026-06-09 22:17:50 +0530

    Add explanations for tuples, lists, mutable vs immutable types, and comprehensions in Python

---
+++ Interview_questions/01_pyhton.py
@@ -0,0 +1,6 @@
+
+'''What is Python?'''
+
+''' Python is a high-level, interpreted, object-oriented, platform-independent, and dynamically-typed programming language. It was created by Guido van Rossum and first released in 1991. Python is known for its simplicity, readability, and versatility, making it a popular choice for beginners and experienced developers alike. it is used in  web development, data analysis, machine learning, artificial intelligence, scientific computing, automation, and many other fields. 
+
+ '''
